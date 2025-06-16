59bc34e8f491fac45025c4cbc576e46a2838362d
HoseinBakhshian <hoseinbakh1379@gmail.com>
2025-06-16 11:00:13 +0200

    2st version, footer fixed

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function footer() {
   return <div className="footer d-flex flex-column align-items-center justify-content-center">
-    <div>Designed and Developed by Hossein Bakhshian</div>
+    <div className="text-center">Designed and Developed by Hossein Bakhshian</div>
     <div>Credit to OMDb API</div>
   </div>;
 }
